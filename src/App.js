9e36c5f4164bbc4b06a9d3b1fd833afcee2b8803
Ionut Matis <ionutmatis16@yahoo.com>
2020-10-06 17:37:37 +0300

    - what happens behind the scenes on App function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div className="App">
+        <h1>Hello, I'm a React app</h1>
+      </div>
+    );
+    // React.createElement(element, config, children);
+    // return React.createElement('div', null, 'h1', 'hello, i\'m a React app');
+    /*return React.createElement('div', {
+            id: 'myDiv',
+            className: 'App'
+        },
+        React.createElement('h1', null, 'Hello, i\'m a React app'));*/
 }
 
 export default App;
